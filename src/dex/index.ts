eec3205915f033719a36db5d96cc11445d084af6
Yun <41171808+sehyunc@users.noreply.github.com>
2025-10-30 17:50:00 -0700

    renegade: add renegade definition

--- src/dex/index.ts
+++ src/dex/index.ts
@@ -117,6 +117,7 @@ import { Blackhole } from './solidly/forks-override/blackhole';
 import { BlackholeCL } from './algebra-integral/forks/blackhole-cl';
 import { BunniV2 } from './bunni-v2/bunni-v2';
 import { Cap } from './cap/cap';
+import { Renegade } from './renegade/renegade';
 
 const LegacyDexes = [
   CurveV2,
@@ -227,6 +228,7 @@ const Dexes = [
   BlackholeCL,
   BunniV2,
   Cap,
+  Renegade,
 ];
 
 export type LegacyDexConstructor = new (dexHelper: IDexHelper) => IDexTxBuilder<
