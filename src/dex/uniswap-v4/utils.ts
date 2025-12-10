cbffc620f5e7553b23cbf7f9bf082cb7fa154169
Ivan Nedokhodiuk <nedokhodiukivan@gmail.com>
2025-12-10 08:58:17 +0200

    feat(utils): add method for PoolKey encoding

--- src/dex/uniswap-v4/utils.ts
+++ src/dex/uniswap-v4/utils.ts
@@ -1,4 +1,5 @@
-import { PoolState } from './types';
+import { ethers } from 'ethers';
+import { PoolKey, PoolState } from './types';
 
 export function sortPools(a: PoolState, b: PoolState) {
   const idA = a.id.toUpperCase();
@@ -10,4 +11,19 @@ export function sortPools(a: PoolState, b: PoolState) {
     return 1;
   }
   return 0;
+}
+
+export function toId(key: PoolKey): string {
+  return ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(
+      ['address', 'address', 'uint24', 'int24', 'address'],
+      [
+        key.currency0.toLowerCase(),
+        key.currency1.toLowerCase(),
+        parseInt(key.fee),
+        key.tickSpacing,
+        key.hooks.toLowerCase(),
+      ],
+    ),
+  );
 }
